b2723792b53ad7a873c29b20305288348295501e
Steven Bradbury <scuba2150@gmail.com>
2024-08-25 13:04:32 +1000

    updates to css

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -5,24 +5,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='wrapper'>
-      <div className='main-header size-100'></div>
-      <div className='main-menu size-100'></div>
-      <div className='main-area size-100'>
-        <div className='top-dashboard'>
-          <div className='top-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='top-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='top-dashboard-display size-100 flex-c'>Hello</div>
-        </div>
-        <div className='middle-dashboard'>
-          <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
-        </div>
-        <div className='bottom-dashboard'>
-          <div className='bottom-dashboard-display size-100 flex-c'>Hello</div>
-          <div className='bottom-dashboard-display size-100 flex-c'>Hello</div>
+    <div className='page-container flex-c'>
+      <div className='wrapper'>
+        <div className='main-header size-100'></div>
+        <div className='main-menu size-100'></div>
+        <div className='main-area size-100'>
+          <div className='top-dashboard'>
+            <div className='top-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='top-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='top-dashboard-display size-100 flex-c'>Hello</div>
+          </div>
+          <div className='middle-dashboard'>
+            <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='middle-dashboard-display size-100 flex-c'>Hello</div>
+          </div>
+          <div className='bottom-dashboard'>
+            <div className='bottom-dashboard-display size-100 flex-c'>Hello</div>
+            <div className='bottom-dashboard-display size-100 flex-c'>Hello</div>
+          </div>
         </div>
       </div>
     </div>
