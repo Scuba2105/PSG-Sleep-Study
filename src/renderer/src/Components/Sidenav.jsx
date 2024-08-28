6aa55cdc8505e942150f8b8629be0d04fa62bc08
Steven Bradbury <scuba2150@gmail.com>
2024-08-28 22:04:51 +1000

    created loading page

--- src/renderer/src/Components/Sidenav.jsx
+++ src/renderer/src/Components/Sidenav.jsx
@@ -12,11 +12,11 @@ export function Sidenav({menuOption, setMenuOption}) {
         <Home color="#F7F7F7"/>
         <h4 className='menu-heading'>Home</h4>
       </div>
-      <div className={menuOption === "Lessons" ? "menu-option-container-selected flex-c" : "menu-option-container flex-c"} style={menuOption === "Lessons" ? {backgroundColor: "#3E64FF", scale: 1.06} : null} onClick={(e) => setOptionSelected(e, setMenuOption)}>
+      <div className={menuOption === "Lessons" ? "menu-option-container-selected flex-c" : "menu-option-container flex-c"} onClick={(e) => setOptionSelected(e, setMenuOption)}>
         <Classes color="#F7F7F7"/>
         <h4 className='menu-heading'>Lessons</h4>
       </div>
-      <div className={menuOption === "Certificates" ? "menu-option-container-selected flex-c" : "menu-option-container flex-c"} style={menuOption === "Certificates" ? {backgroundColor: "#3E64FF", scale: 1.06} : null} onClick={(e) => setOptionSelected(e, setMenuOption)}>
+      <div className={menuOption === "Certificates" ? "menu-option-container-selected flex-c" : "menu-option-container flex-c"} onClick={(e) => setOptionSelected(e, setMenuOption)}>
         <Certificate color="#F7F7F7"/>
         <h4 className='menu-heading'>Certificates</h4>
       </div>
